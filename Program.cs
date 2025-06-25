ea730776f87ce89b4ae0b42eb307ef98cccb6518
UMA <cadman77@yandex.ru>
2025-06-25 14:52:52 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿// Grok-generated
+//
+// My Prompt:
+// C# console multi thread program to log to C:\Temp\time.txt:
+// - main thread (each 10 sec) log DateTime of getting last correct answer from NTP-server;
+// - auxiliary thread (each 60 sec) request (time.windows.com) and log current UTC DateTime (or error message).
+// Including restart of auxiliary thread in case of crash (log the crash to log)
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static readonly string LogFilePath = @"C:\Temp\time.txt";
+    private static DateTime? LastNtpTime = null;
+    private static readonly object FileLock = new object();
+
+    static async Task Main(string[] args)
+    {
+        Directory.CreateDirectory(Path.GetDirectoryName(LogFilePath));
+
+        // Start auxiliary thread for NTP requests
+        _ = Task.Run(() => ManageNtpThread());
+
+        // Main thread: log last NTP time every 10 seconds
+        while (true)
+        {
+            LogMessage($"Main thread: Last NTP time: {(LastNtpTime.HasValue ? LastNtpTime.Value.ToString("u") : "None")}");
+            await Task.Delay(10000);
+        }
+    }
+
+    static void ManageNtpThread()
+    {
+        while (true)
+        {
+            try
+            {
+                Thread ntpThread = new Thread(NtpRequestLoop)
+                {
+                    IsBackground = true
+                };
+                ntpThread.Start();
+                ntpThread.Join(); // Wait for thread to exit
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"NTP thread crashed: {ex.Message}");
+            }
+            Thread.Sleep(5000); // Wait before restarting
+        }
+    }
+
+    static void NtpRequestLoop()
+    {
+        while (true)
+        {
+            try
+            {
+                DateTime ntpTime = GetNtpTime("time.windows.com");
+                LastNtpTime = ntpTime;
+                LogMessage($"NTP thread: UTC time from server: {ntpTime:u}");
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"NTP thread error: {ex.Message}");
+            }
+            Thread.Sleep(60000); // Wait 60 seconds
+        }
+    }
+
+    static DateTime GetNtpTime(string ntpServer)
+    {
+        byte[] ntpData = new byte[48];
+        ntpData[0] = 0x1B; // NTP request header
+
+        using (var client = new UdpClient(ntpServer, 123))
+        {
+            client.Send(ntpData, ntpData.Length);
+            // ntpData = client.Receive(ref client.Client.RemoteEndPoint);
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0); // Initialize endpoint
+            ntpData = client.Receive(ref remoteEndPoint); // Pass the IPEndPoint
+
+            // Parse NTP response (seconds since 1900)
+            ulong intPart = BitConverter.ToUInt32(ntpData, 40);
+            intPart = (uint)((intPart >> 24) | ((intPart >> 8) & 0xFF00) | ((intPart << 8) & 0xFF0000) | (intPart << 24));
+            double seconds = intPart;
+
+            // Convert to DateTime
+            DateTime ntpEpoch = new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return ntpEpoch.AddSeconds(seconds);
+        }
+    }
+
+    static void LogMessage(string message)
+    {
+        lock (FileLock)
+        {
+            string logEntry = $"{DateTime.UtcNow:u}: {message}{Environment.NewLine}";
+            File.AppendAllText(LogFilePath, logEntry);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UTC-DateTime-from-Win-NTP-MultiThread")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UTC-DateTime-from-Win-NTP-MultiThread")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b1b74985-0149-40ef-b423-dede418e0c82")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
